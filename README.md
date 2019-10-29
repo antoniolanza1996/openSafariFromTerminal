e9267e640b570afa9b79455030319cdc200a326e
Antonio Lanza <antoniolanza1996@gmail.com>
2019-10-29 10:01:57 +0100

    add script and README

---
+++ README.md
@@ -0,0 +1,15 @@
+# openSafariFromTerminal
+#### Useful script to open and interact with Safari from terminal on MacOS 
+
+## 3 types of usage: 
+1. ```safari.sh <without parameters>``` ----> simply open Safari
+2. ```safari.sh --url URL_PARAM``` ----> open a tab in Safari with the site specified in URL_PARAM (NOTE: You should write your URL without specifing the protocol)
+3. ```safari.sh YOUR_QUERY_SEPARATED_BY_SPACE```  ----> open Safari and search "YOUR_QUERY_SEPARATED_BY_SPACE" on Google
+
+## Examples of usage: 
+
+```safari.sh --url www.github.com```  ----> open https://www.github.com on Safari
+
+```safari.sh something to search on Google``` ----> find "something to search on Google" on Google 
+
+
---
+++ safari.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Useful script to open and interact with Safari from terminal on MacOS 
+
+#3 types of usage: 
+# 1. safari.sh <without parameters> ----> simply open Safari 
+# 2. safari.sh --url URL_PARAM ----> open a tab in Safari with the site specified in URL_PARAM (NOTE: You should write your URL without specifing the protocol)
+# 3. safari.sh YOUR_QUERY_SEPARATED_BY_SPACE  ----> open Safari and search "YOUR_QUERY_SEPARATED_BY_SPACE" on Google
+
+#Examples of usage: 
+# safari.sh --url www.github.com  ----> open https://www.github.com on Safari
+# safari.sh something to search on Google ----> find "something to search on Google" on Google 
+
+if [ $# -eq 0 ];then
+	echo "[++++++++++++ You are opening Safari... goodbye! ++++++++++++]"
+	open -a Safari
+else
+	URL_attribute="--url"
+	if [ $1 == $URL_attribute ];then 
+		echo "[++++++++++++ You are opening Safari with a personalized URL... goodbye! ++++++++++++]"
+		open -a Safari https://$2 
+	else
+		complete_query_string="https://www.google.com/search?q=" #format: https://www.google.com/search?q=something+to+search+on+Google
+		partial_query_string=""
+		for i in $@; do
+			partial_query_string=$partial_query_string+$i
+		done
+		partial_query_string=${partial_query_string:1} #useful for delete the first "+" 
+		complete_query_string=$complete_query_string$partial_query_string
+		echo "[++++++++++++ You are opening Safari with a personalized query... goodbye! ++++++++++++]"
+		open -a Safari $complete_query_string
+	fi
+fi
